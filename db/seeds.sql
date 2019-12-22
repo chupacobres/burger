867477d7685a7b70824884e793658d96b70c9be9
juan uribe <juanpablouribe@mac.com>
2019-12-21 23:46:18 -0500

    created databases with 6 different burgers to start the page

---
+++ db/seeds.sql
@@ -0,0 +1,33 @@
+INSERT INTO burgers (
+burger_name,
+devoured
+) VALUES
+(
+"Vegan Burger",
+false
+),
+(
+"Lamb Burger",
+false
+),
+(
+"Mexican Burger",
+true
+);
+(
+"Roxy Burger",
+true
+);
+(
+"Grande Pork Burger",
+false
+);
+(
+"Niki Minaj Burger",
+0
+false
+);
+(
+"Skinny Burger",
+false
+);
